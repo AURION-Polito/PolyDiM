9272ac82975fdd1b31f5fbf6d20eedda1f968169
fabio.vicini <fabio.vicini@polito.it>
2024-12-13 17:38:53 +0100

    add: program utilities

--- PolyDiM/examples/Elliptic_PCC_2D/main.cpp
+++ PolyDiM/examples/Elliptic_PCC_2D/main.cpp
@@ -7,6 +7,8 @@
 #include "Eigen_CholeskySolver.hpp"
 #include "assembler.hpp"
 #include "MeshMatricesDAO_mesh_connectivity_data.hpp"
+#include "VEM_PCC_2D_LocalSpace.hpp"
+#include "program_utilities.hpp"
 
 struct PatchTest final
 {
@@ -192,7 +194,7 @@ unsigned int PatchTest::order;
 
 int main(int argc, char** argv)
 {
-  Elliptic_PCC_2D::Program_configuration config;
+  Polydim::examples::Elliptic_PCC_2D::Program_configuration config;
 
   if (!Gedim::Output::FileExists("./Parameters.ini"))
     Gedim::Configurations::ExportToIni("./Parameters.ini",
@@ -201,12 +203,6 @@ int main(int argc, char** argv)
     Gedim::Configurations::InitializeFromIni("./Parameters.ini");
 
   Gedim::Configurations::Initialize(argc, argv);
-
-  Gedim::GeometryUtilitiesConfig geometryUtilitiesConfig;
-  geometryUtilitiesConfig.Tolerance1D = config.GeometricTolerance();
-  Gedim::GeometryUtilities geometryUtilities(geometryUtilitiesConfig);
-
-  Gedim::MeshUtilities meshUtilities;
 
   /// Create folders
   const string exportFolder = config.ExportFolder();
@@ -264,40 +260,17 @@ int main(int argc, char** argv)
   Gedim::MeshMatrices meshData;
   Gedim::MeshMatricesDAO mesh(meshData);
 
-  switch (config.MeshGenerator())
-  {
-    case Polydim::PDETools::Mesh::PDE_Mesh_Utilities::MeshGenerator_Types_2D::Triangular:
-    case Polydim::PDETools::Mesh::PDE_Mesh_Utilities::MeshGenerator_Types_2D::Minimal:
-    case Polydim::PDETools::Mesh::PDE_Mesh_Utilities::MeshGenerator_Types_2D::Polygonal:
-    {
-      Polydim::PDETools::Mesh::PDE_Mesh_Utilities::create_mesh_2D(geometryUtilities,
-                                                                  meshUtilities,
-                                                                  config.MeshGenerator(),
-                                                                  domain,
-                                                                  config.MeshMaxArea(),
-                                                                  mesh);
-    }
-      break;
-    case Polydim::PDETools::Mesh::PDE_Mesh_Utilities::MeshGenerator_Types_2D::OFFImporter:
-    {
-      Polydim::PDETools::Mesh::PDE_Mesh_Utilities::import_mesh_2D(geometryUtilities,
-                                                                  meshUtilities,
-                                                                  config.MeshGenerator(),
-                                                                  config.MeshImportFilePath(),
-                                                                  mesh);
-    }
-      break;
-    default:
-      throw runtime_error("MeshGenerator " +
-                          to_string((unsigned int)config.MeshGenerator()) +
-                          " not supported");
-  }
+
+  Polydim::examples::Elliptic_PCC_2D::program_utilities::create_domain_mesh(config,
+                                                                            domain,
+                                                                            mesh);
 
   Gedim::Profiler::StopTime("CreateMesh");
   Gedim::Output::PrintStatusProgram("CreateMesh");
 
   // Export the domain mesh
   {
+    Gedim::MeshUtilities meshUtilities;
     meshUtilities.ExportMeshToVTU(mesh,
                                   exportVtuFolder,
                                   "Domain_Mesh");
@@ -306,9 +279,9 @@ int main(int argc, char** argv)
   Gedim::Output::PrintGenericMessage("ComputeGeometricProperties...", true);
   Gedim::Profiler::StartTime("ComputeGeometricProperties");
 
-  const auto meshGeometricData = Polydim::PDETools::Mesh::PDE_Mesh_Utilities::compute_mesh_2D_geometry_data(geometryUtilities,
-                                                                                                            meshUtilities,
-                                                                                                            mesh);
+  const auto meshGeometricData = Polydim::examples::Elliptic_PCC_2D::program_utilities::create_domain_mesh_geometric_properties(config,
+                                                                                                                                mesh);
+
   Gedim::Profiler::StopTime("ComputeGeometricProperties");
   Gedim::Output::PrintStatusProgram("ComputeGeometricProperties");
 
@@ -347,18 +320,18 @@ int main(int argc, char** argv)
   Gedim::Output::PrintGenericMessage("AssembleSystem VEM Type " + to_string((unsigned int)config.VemType()) + "...", true);
   Gedim::Profiler::StartTime("AssembleSystem");
 
-  Elliptic_PCC_2D::Assembler assembler;
+  Polydim::VEM::PCC::VEM_PCC_2D_LocalSpace vem_local_space;
 
-  auto assembler_data = assembler.Assemble(geometryUtilities,
-                                           mesh,
-                                           meshGeometricData,
-                                           meshDOFsInfo,
-                                           dofs_data,
-                                           reference_element_data,
-                                           diffusion_term,
-                                           source_term,
-                                           strong_boundary_condition,
-                                           weak_boundary_condition);
+  auto assembler_data = Polydim::examples::Elliptic_PCC_2D::Assembler::Assemble(mesh,
+                                                                                meshGeometricData,
+                                                                                meshDOFsInfo,
+                                                                                dofs_data,
+                                                                                reference_element_data,
+                                                                                vem_local_space,
+                                                                                diffusion_term,
+                                                                                source_term,
+                                                                                strong_boundary_condition,
+                                                                                weak_boundary_condition);
 
   Gedim::Profiler::StopTime("AssembleSystem");
   Gedim::Output::PrintStatusProgram("AssembleSystem");
@@ -388,14 +361,14 @@ int main(int argc, char** argv)
   Gedim::Output::PrintGenericMessage("ComputeErrors...", true);
   Gedim::Profiler::StartTime("ComputeErrors");
 
-  auto post_process_data = assembler.PostProcessSolution(geometryUtilities,
-                                                         mesh,
-                                                         meshGeometricData,
-                                                         dofs_data,
-                                                         reference_element_data,
-                                                         assembler_data,
-                                                         exact_solution,
-                                                         exact_derivative_solution);
+  auto post_process_data = Polydim::examples::Elliptic_PCC_2D::Assembler::PostProcessSolution(mesh,
+                                                                                              meshGeometricData,
+                                                                                              dofs_data,
+                                                                                              reference_element_data,
+                                                                                              vem_local_space,
+                                                                                              assembler_data,
+                                                                                              exact_solution,
+                                                                                              exact_derivative_solution);
 
   Gedim::Profiler::StopTime("ComputeErrors");
   Gedim::Output::PrintStatusProgram("ComputeErrors");
@@ -403,114 +376,13 @@ int main(int argc, char** argv)
   Gedim::Output::PrintGenericMessage("ExportSolution...", true);
   Gedim::Profiler::StartTime("ExportSolution");
 
-  {
-    const char separator = ';';
-
-    std::cout<< "VemType" << separator;
-    std::cout<< "VemOrder" << separator;
-    std::cout<< "Cell2Ds" <<  separator;
-    std::cout<< "Dofs" <<  separator;
-    std::cout<< "Strongs" <<  separator;
-    std::cout<< "h" <<  separator;
-    std::cout<< "errorL2" <<  separator;
-    std::cout<< "errorH1" << separator;
-    std::cout<< "normL2" <<  separator;
-    std::cout<< "normH1" << separator;
-    std::cout<< "nnzA" << separator;
-    std::cout<< "residual" << std::endl;
-
-    std::cout.precision(2);
-    std::cout<< scientific<< static_cast<unsigned int>(config.VemType())<< separator;
-    std::cout<< scientific<< config.VemOrder()<< separator;
-    std::cout<< scientific<< mesh.Cell2DTotalNumber()<< separator;
-    std::cout<< scientific<< dofs_data.NumberDOFs<< separator;
-    std::cout<< scientific<< dofs_data.NumberStrongs<< separator;
-    std::cout<< scientific<< post_process_data.mesh_size << separator;
-    std::cout<< scientific<< post_process_data.error_L2<< separator;
-    std::cout<< scientific<< post_process_data.error_H1<< separator;
-    std::cout<< scientific<< post_process_data.norm_L2<< separator;
-    std::cout<< scientific<< post_process_data.norm_H1<< separator;
-    std::cout<< scientific<< assembler_data.globalMatrixA.NonZeros()<< separator;
-    std::cout<< scientific<< post_process_data.residual_norm<< std::endl;
-  }
-
-  {
-    const char separator = ';';
-    const string errorFileName = exportSolutionFolder +
-                                 "/Errors.csv";
-    const bool errorFileExists = Gedim::Output::FileExists(errorFileName);
-
-    std::ofstream errorFile(errorFileName,
-                            std::ios_base::app | std::ios_base::out);
-    if (!errorFileExists)
-    {
-      errorFile<< "VemType" << separator;
-      errorFile<< "VemOrder" << separator;
-      errorFile<< "Cell2Ds" <<  separator;
-      errorFile<< "Dofs" <<  separator;
-      errorFile<< "Strongs" <<  separator;
-      errorFile<< "h" <<  separator;
-      errorFile<< "errorL2" <<  separator;
-      errorFile<< "errorH1" << separator;
-      errorFile<< "normL2" <<  separator;
-      errorFile<< "normH1" << separator;
-      errorFile<< "nnzA" << separator;
-      errorFile<< "residual" << std::endl;
-    }
-
-    errorFile.precision(16);
-    errorFile<< scientific<< static_cast<unsigned int>(config.VemType())<< separator;
-    errorFile<< scientific<< config.VemOrder()<< separator;
-    errorFile<< scientific<< mesh.Cell2DTotalNumber()<< separator;
-    errorFile<< scientific<< dofs_data.NumberDOFs<< separator;
-    errorFile<< scientific<< dofs_data.NumberStrongs<< separator;
-    errorFile<< scientific<< post_process_data.mesh_size << separator;
-    errorFile<< scientific<< post_process_data.error_L2<< separator;
-    errorFile<< scientific<< post_process_data.error_H1<< separator;
-    errorFile<< scientific<< post_process_data.norm_L2<< separator;
-    errorFile<< scientific<< post_process_data.norm_H1<< separator;
-    errorFile<< scientific<< assembler_data.globalMatrixA.NonZeros()<< separator;
-    errorFile<< scientific<< post_process_data.residual_norm<< std::endl;
-
-    errorFile.close();
-  }
-
-  {
-    {
-      Gedim::VTKUtilities exporter;
-      exporter.AddPolygons(mesh.Cell0DsCoordinates(),
-                           mesh.Cell2DsVertices(),
-                           {
-                             {
-                               "Numeric",
-                               Gedim::VTPProperty::Formats::Points,
-                               static_cast<unsigned int>(post_process_data.cell0Ds_numeric.size()),
-                               post_process_data.cell0Ds_numeric.data()
-                             },
-                             {
-                               "Exact",
-                               Gedim::VTPProperty::Formats::Points,
-                               static_cast<unsigned int>(post_process_data.cell0Ds_exact.size()),
-                               post_process_data.cell0Ds_exact.data()
-                             },
-                             {
-                               "ErrorL2",
-                               Gedim::VTPProperty::Formats::Cells,
-                               static_cast<unsigned int>(post_process_data.cell2Ds_error_L2.size()),
-                               post_process_data.cell2Ds_error_L2.data()
-                             },
-                             {
-                               "ErrorH1",
-                               Gedim::VTPProperty::Formats::Cells,
-                               static_cast<unsigned int>(post_process_data.cell2Ds_error_H1.size()),
-                               post_process_data.cell2Ds_error_H1.data()
-                             }
-                           });
-
-      exporter.Export(exportVtuFolder + "/Solution_Cell2Ds.vtu");
-    }
-  }
-
+  Polydim::examples::Elliptic_PCC_2D::program_utilities::export_solution(config,
+                                                                         mesh,
+                                                                         dofs_data,
+                                                                         assembler_data,
+                                                                         post_process_data,
+                                                                         exportSolutionFolder,
+                                                                         exportVtuFolder);
   Gedim::Profiler::StopTime("ExportSolution");
   Gedim::Output::PrintStatusProgram("ExportSolution");
 
@@ -519,10 +391,10 @@ int main(int argc, char** argv)
 
   if (config.ComputeVEMPerformance())
   {
-    const auto vemPerformance = assembler.ComputeVemPerformance(geometryUtilities,
-                                                                mesh,
-                                                                meshGeometricData,
-                                                                reference_element_data);
+    const auto vemPerformance = Polydim::examples::Elliptic_PCC_2D::Assembler::ComputeVemPerformance(mesh,
+                                                                                                     meshGeometricData,
+                                                                                                     reference_element_data,
+                                                                                                     vem_local_space);
     {
       const char separator = ',';
       /// Export Cell2Ds VEM performance
