4facee9d74fb8f3ef928e55f3470569e1f9b1958
fabio.vicini <fabio.vicini@polito.it>
2024-12-23 07:50:35 +0100

    still

--- PolyDiM/examples/Elliptic_PCC_2D/integration_test.py
+++ PolyDiM/examples/Elliptic_PCC_2D/integration_test.py
@@ -1,5 +1,6 @@
 import os
 import csv
+import math
 
 def run_program(program_folder,
                 program_path,
@@ -52,19 +53,45 @@ def import_errors(export_path):
         file_reader = csv.reader(csvfile, delimiter=';')
         data = list(file_reader)        
 
+        counter = 0
         for row in data:
             errors_row = []
-            errors_row.append(row[4])
-            errors_row.append(row[7])
-            errors_row.append(row[8])
-            errors_row.append(row[9])
-            errors_row.append(row[10])
+            if counter == 0:
+                errors_row.append(row[4])
+                errors_row.append(row[7])
+                errors_row.append(row[8])
+                errors_row.append(row[9])
+                errors_row.append(row[10])
+            else:
+                errors_row.append(float(row[4]))
+                errors_row.append(float(row[7]))
+                errors_row.append(float(row[8]))
+                errors_row.append(float(row[9]))
+                errors_row.append(float(row[10]))
             errors.append(errors_row)
+            counter += 1
 
     return errors  
 
-def test_errors(errors):
-    exit()  
+def test_errors(errors,
+                vem_order,
+                tol):
+    print(errors)
+    num_rows = len(errors)
+
+    if (num_rows == 2):
+        assert abs(errors[1][1]) < tol * abs(errors[1][3])
+        assert abs(errors[1][2]) < tol * abs(errors[1][4])
+    elif (num_rows == 3):
+        slope_L2 = abs(math.log(errors[2][1] / errors[2][3])  - math.log(errors[1][1] / errors[2][3])) / \
+        abs(math.log(errors[2][0]) - math.log(errors[1][0])) * (-2.0)
+        slope_H1 = abs(math.log(errors[2][2] / errors[2][4])  - math.log(errors[1][2] / errors[2][4])) / \
+        abs(math.log(errors[2][0]) - math.log(errors[1][0])) * (-2.0)
+        print(slope_L2, slope_H1)
+        assert slope_L2 < tol * float(vem_order + 1.0)
+        assert slope_H1 < tol * float(vem_order)
+    else:
+        raise Exception("Case {0} not managed".format(num_rows))
 
 if __name__ == "__main__":
     program_folder = os.path.dirname(os.path.realpath(__file__))
@@ -73,6 +100,9 @@ if __name__ == "__main__":
     vem_types = [1, 2, 3]
     vem_orders = [1, 2, 3]
     export_folder = "integration_tests"
+    tol = 1.0e-8
+
+    print("RUN TESTS...")
 
     test_type = 1
     mesh_generator = 1
@@ -88,7 +118,9 @@ if __name__ == "__main__":
                                       mesh_generator,
                                       mesh_max_area)
             errors = import_errors(export_path)
-            test_errors(errors)
+            test_errors(errors,
+                        vem_order,
+                        tol)
             
     test_type = 2
     mesh_generator = 0
@@ -104,6 +136,10 @@ if __name__ == "__main__":
                             test_type,
                             mesh_generator,
                             mesh_max_area)
+                errors = import_errors(export_path)
+                test_errors(errors,
+                            vem_order,
+                            tol)
     
     test_type = 2
     mesh_generator = 2
@@ -119,6 +155,11 @@ if __name__ == "__main__":
                             test_type,
                             mesh_generator,
                             mesh_max_area)
+                errors = import_errors(export_path)
+                test_errors(errors,
+                            vem_order,
+                            tol)
     
     os.system("rm -rf " + os.path.join(program_folder, export_folder))
+    print("TESTS SUCCESS")
 
