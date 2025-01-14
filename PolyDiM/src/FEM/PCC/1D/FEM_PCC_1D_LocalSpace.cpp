874f0812152a743f538b0050200d539d9ce3eb27
fabio.vicini <fabio.vicini@polito.it>
2025-01-14 10:12:44 +0100

    fix 3D

--- PolyDiM/src/FEM/PCC/1D/FEM_PCC_1D_LocalSpace.cpp
+++ PolyDiM/src/FEM/PCC/1D/FEM_PCC_1D_LocalSpace.cpp
@@ -22,6 +22,28 @@ FEM_PCC_1D_LocalSpace_Data FEM_PCC_1D_LocalSpace::CreateLocalSpace(const FEM_PCC
     localSpace.Order = reference_element_data.Order;
     localSpace.NumberOfBasisFunctions = reference_element_data.NumBasisFunctions;
 
+    localSpace.DofsMeshOrder.resize(localSpace.NumberOfBasisFunctions, 0);
+    unsigned int dofCounter = 0;
+    localSpace.Dof0DsIndex.fill(0);
+    for (unsigned int v = 0; v < 2; v++)
+    {
+        localSpace.Dof0DsIndex[v + 1] = localSpace.Dof0DsIndex[v] + reference_element_data.NumDofs0D;
+
+        for (unsigned int d = localSpace.Dof0DsIndex[v]; d < localSpace.Dof0DsIndex[v + 1]; d++)
+        {
+            localSpace.DofsMeshOrder[dofCounter] = d;
+            dofCounter++;
+        }
+    }
+
+    localSpace.Dof1DsIndex.fill(localSpace.Dof0DsIndex[3]);
+    localSpace.Dof1DsIndex[1] = localSpace.Dof1DsIndex[0] + reference_element_data.NumDofs1D;
+    for (unsigned int d = localSpace.Dof1DsIndex[0]; d < localSpace.Dof1DsIndex[1]; d++)
+    {
+        localSpace.DofsMeshOrder[dofCounter] = d;
+        dofCounter++;
+    }
+
     // reorder basis function values with mesh order
     localSpace.Dofs = MapValues(localSpace, F(localSpace.MapData, reference_element_data.DofPositions));
 
@@ -30,12 +52,41 @@ FEM_PCC_1D_LocalSpace_Data FEM_PCC_1D_LocalSpace::CreateLocalSpace(const FEM_PCC
     return localSpace;
 }
 // ***************************************************************************
+MatrixXd FEM_PCC_1D_LocalSpace::MapValues(const FEM_PCC_1D_LocalSpace_Data& local_space, const Eigen::MatrixXd& referenceValues) const
+{
+  Eigen::MatrixXd basisFunctionValuesOrdered(referenceValues.rows(), local_space.NumberOfBasisFunctions);
+
+  for (unsigned int d = 0; d < local_space.NumberOfBasisFunctions; d++)
+      basisFunctionValuesOrdered.col(local_space.DofsMeshOrder.at(d)) << referenceValues.col(d);
+
+  return basisFunctionValuesOrdered;
+}
+// ***************************************************************************
+std::vector<MatrixXd> FEM_PCC_1D_LocalSpace::MapDerivativeValues(const FEM_PCC_1D_LocalSpace_Data& local_space, const std::vector<Eigen::MatrixXd>& referenceDerivateValues) const
+{
+  std::vector<Eigen::MatrixXd> basisFunctionsDerivativeValues(1);
+
+  basisFunctionsDerivativeValues[0] = referenceDerivateValues[0] / local_space.MapData.Length;
+
+  std::vector<Eigen::MatrixXd> basisFunctionsDerivativeValuesOrdered(
+      1,
+      Eigen::MatrixXd(referenceDerivateValues.at(0).rows(), local_space.NumberOfBasisFunctions));
+
+  for (unsigned int d = 0; d < local_space.NumberOfBasisFunctions; d++)
+  {
+      const unsigned int &dofOrder = local_space.DofsMeshOrder.at(d);
+      basisFunctionsDerivativeValuesOrdered.at(0).col(dofOrder) << basisFunctionsDerivativeValues.at(0).col(d);
+  }
+
+  return basisFunctionsDerivativeValuesOrdered;
+}
+// ***************************************************************************
 Gedim::Quadrature::QuadratureData FEM_PCC_1D_LocalSpace::InternalQuadrature(const Gedim::Quadrature::QuadratureData &reference_quadrature,
                                                                             const FEM_PCC_1D_LocalSpace_Data::SegmentMapData &mapData) const
 {
-    Gedim::Quadrature::QuadratureData quadrature;
+  Gedim::Quadrature::QuadratureData quadrature;
 
-    quadrature.Points = F(mapData, reference_quadrature.Points);
+  quadrature.Points = F(mapData, reference_quadrature.Points);
     quadrature.Weights = reference_quadrature.Weights.array() * DetJ(mapData, reference_quadrature.Points).array().abs();
 
     return quadrature;
