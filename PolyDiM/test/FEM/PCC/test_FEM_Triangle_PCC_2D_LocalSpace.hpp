aaee019c9f2f379673cb12cc3107edaab2ca6a88
fabio.vicini <fabio.vicini@polito.it>
2025-01-16 12:56:40 +0100

    still

--- PolyDiM/test/FEM/PCC/test_FEM_Triangle_PCC_2D_LocalSpace.hpp
+++ PolyDiM/test/FEM/PCC/test_FEM_Triangle_PCC_2D_LocalSpace.hpp
@@ -20,16 +20,6 @@ namespace Polydim
       const auto referenceQuadrature = Gedim::Quadrature::Quadrature_Gauss2D_Triangle::FillPointsAndWeights(21);
       const Eigen::MatrixXd &referenceQuadraturePoints = referenceQuadrature.Points;
 
-      std::array<Eigen::Vector3d, 3> boundaries_normal;
-      boundaries_normal[0] = Eigen::Vector3d(0.0, -1.0, 0.0).normalized();
-      boundaries_normal[1] = Eigen::Vector3d(+1.0, +1.0, 0.0).normalized();
-      boundaries_normal[1] = Eigen::Vector3d(-1.0, 0.0, 0.0).normalized();
-      std::array<Gedim::Quadrature::QuadratureData, 3> boundaries_quadrature;
-      boundaries_quadrature[0].Points = Eigen::Vector3d(0.0, 0.0, 0.0);
-      boundaries_quadrature[0].Weights = Eigen::VectorXd::Constant(1, 1.0);
-      boundaries_quadrature[1].Points = Eigen::Vector3d(1.0, 0.0, 0.0);
-      boundaries_quadrature[1].Weights = Eigen::VectorXd::Constant(1, 1.0);
-
       for (unsigned int o = 0; o < 5; o++)
       {
         const auto reference_element_data = reference_element.Create(o);
@@ -64,25 +54,6 @@ namespace Polydim
           ASSERT_TRUE(abs(sumDerYYValues[q]) < 1.0e-14);
         }
 
-        const auto& internal_quadrature = reference_element_data.ReferenceTriangleQuadrature;
-        const auto& derivative_values = reference_element_data.ReferenceBasisFunctionDerivativeValues;
-
-        Eigen::VectorXd internal_integral = Eigen::VectorXd::Zero(reference_element_data.NumBasisFunctions);
-        for (unsigned int dim = 0; dim < reference_element_data.Dimension; ++dim)
-          internal_integral += derivative_values[dim].transpose() * internal_quadrature.Weights;
-
-        Eigen::VectorXd boundary_integral = Eigen::VectorXd::Zero(reference_element_data.NumBasisFunctions);
-        for (unsigned int b = 0; b < boundaries_normal.size(); ++b)
-        {
-          const Eigen::Vector3d boundary_normal = boundaries_normal[b];
-          const auto& boundary_quadrature = boundaries_quadrature[b];
-          const auto boundary_values = reference_element.EvaluateBasisFunctions(boundary_quadrature.Points,
-                                                                                reference_element_data);
-          boundary_integral += boundary_values.transpose() *
-                               boundary_quadrature.Weights *
-                               boundary_normal.sum();
-        }
-        ASSERT_TRUE((internal_integral - boundary_integral).norm() < 1.0e-14 * std::max(1.0, boundary_integral.norm()));
       }
     }
 
@@ -128,12 +99,12 @@ namespace Polydim
         points << dofs, internal_quadrature.Points;
 
         const auto basisValues = local_space.ComputeBasisFunctionsValues(reference_element_data,
-                                                                                    local_space_data,
-                                                                                    points);
+                                                                         local_space_data,
+                                                                         points);
         const auto gradBasisValues =
             local_space.ComputeBasisFunctionsDerivativeValues(reference_element_data,
-                                                    local_space_data,
-                                                    points);
+                                                              local_space_data,
+                                                              points);
 
         const Eigen::VectorXd sumBasisValues = basisValues.rowwise().sum();
         const Eigen::VectorXd sumGradXValues = gradBasisValues[0].rowwise().sum();
@@ -147,8 +118,8 @@ namespace Polydim
         }
 
         const auto& derivative_values = local_space.ComputeBasisFunctionsDerivativeValues(reference_element_data,
-                                                                                local_space_data,
-                                                                                internal_quadrature.Points);
+                                                                                          local_space_data,
+                                                                                          internal_quadrature.Points);
 
         Eigen::VectorXd internal_integral = Eigen::VectorXd::Zero(reference_element_data.NumBasisFunctions);
         for (unsigned int dim = 0; dim < reference_element_data.Dimension; ++dim)
