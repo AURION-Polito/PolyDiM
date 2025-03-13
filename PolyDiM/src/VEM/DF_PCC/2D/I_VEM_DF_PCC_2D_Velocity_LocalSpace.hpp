38566a49bd165306108b769dd72989a880654eaf
Andrea Borio <andrea.borio@polito.it>
2025-03-13 18:45:09 +0100

    Docs of I_VEM_DF_PCC_2D_Velocity_LocalSpace

--- PolyDiM/src/VEM/DF_PCC/2D/I_VEM_DF_PCC_2D_Velocity_LocalSpace.hpp
+++ PolyDiM/src/VEM/DF_PCC/2D/I_VEM_DF_PCC_2D_Velocity_LocalSpace.hpp
@@ -61,13 +61,27 @@ class I_VEM_DF_PCC_2D_Velocity_LocalSpace
     virtual std::vector<Eigen::MatrixXd> ComputeBasisFunctionsDerivativeValues(const VEM_DF_PCC_2D_Velocity_LocalSpace_Data &localSpace,
                                                                                const ProjectionTypes &projectionType) const = 0;
 
-
+    /// \brief Compute values of a suitable projection of basis functions at default quadrature points.
+    /// \param reference_element_data Data of the reference element of the VEM space.
+    /// \param polygon The geometry of the polygon.
+    /// \param localSpace Data of the local space.
+    /// \param projectionType Type of projection operator to be applied.
+    /// \param points Evaluation points.
+    /// \return A vector of two matrices containing the values of the two components of a basis function's projection on
+    /// each column.
     virtual std::vector<Eigen::MatrixXd> ComputeBasisFunctionsValues(const VEM_DF_PCC_2D_Velocity_ReferenceElement_Data &reference_element_data,
                                                                      const VEM_DF_PCC_2D_Polygon_Geometry &polygon,
                                                                      const VEM_DF_PCC_2D_Velocity_LocalSpace_Data &localSpace,
                                                                      const ProjectionTypes &projectionType,
                                                                      const Eigen::MatrixXd &points) const = 0;
 
+    /// \brief Compute values of projected derivatives of each component of a basis function at default quadrature points.
+    /// \param reference_element_data Data of the reference element of the VEM space.
+    /// \param polygon The geometry of the polygon.
+    /// \param localSpace Data of the local space.
+    /// \param projectionType Type of projection operator to be applied.
+    /// \param points Evaluation points.
+    /// \return A vector of four components such that the 2 * j + i component is the i-th derivative of the j-th component.
     virtual std::vector<Eigen::MatrixXd> ComputeBasisFunctionsDerivativeValues(const VEM_DF_PCC_2D_Velocity_ReferenceElement_Data &reference_element_data,
                                                                                const VEM_DF_PCC_2D_Polygon_Geometry &polygon,
                                                                                const VEM_DF_PCC_2D_Velocity_LocalSpace_Data &localSpace,
@@ -88,16 +102,32 @@ class I_VEM_DF_PCC_2D_Velocity_LocalSpace
                                                      const VEM_DF_PCC_2D_Polygon_Geometry &polygon,
                                                      const Eigen::MatrixXd &points) const = 0;
 
+    /// \brief Compute values of the derivatives of the polynomial basis at default quadrature points.
+    /// \param localSpace Data of the local space.
+    /// \return A vector of two matrices, each one containing values of derivatives on columns.
     virtual std::vector<Eigen::MatrixXd> ComputePolynomialsDerivativeValues(const VEM_DF_PCC_2D_Velocity_LocalSpace_Data &localSpace) const = 0;
 
+    /// \brief Compute values of the derivatives of the polynomial basis at given points.
+    /// \param reference_element_data Data of the reference element of the VEM space.
+    /// \param polygon Polygon geometry.
+    /// \param points Evaluation points.
+    /// \return A vector of two matrices, each one containing values of derivatives on columns.
     virtual std::vector<Eigen::MatrixXd> ComputePolynomialsDerivativeValues(const VEM_DF_PCC_2D_Velocity_ReferenceElement_Data &reference_element_data,
                                                                             const VEM_DF_PCC_2D_Polygon_Geometry &polygon,
                                                                             const Eigen::MatrixXd &points) const = 0;
 
+    /// \brief Compute values of the laplacian of the polynomial basis at given points.
+    /// \param reference_element_data Data of the reference element of the VEM space.
+    /// \param polygon Polygon geometry.
+    /// \param points Evaluation points.
+    /// \return A matrix containing the values of the laplacian of a basis function on each column.
     virtual Eigen::MatrixXd ComputePolynomialsLaplacianValues(const VEM_DF_PCC_2D_Velocity_ReferenceElement_Data &reference_element_data,
                                                               const VEM_DF_PCC_2D_Polygon_Geometry &polygon,
                                                               const Eigen::MatrixXd &points) const = 0;
 
+    /// \brief Compute values of the divergence of basis functions at default quadrature points.
+    /// \param localSpace Data of the local space.
+    /// \return A matrix containing the values of the divergence of basis function on each column.
     virtual Eigen::MatrixXd ComputeBasisFunctionsDivergenceValues(const VEM_DF_PCC_2D_Velocity_LocalSpace_Data &localSpace) const = 0;
 };
 } // namespace DF_PCC
