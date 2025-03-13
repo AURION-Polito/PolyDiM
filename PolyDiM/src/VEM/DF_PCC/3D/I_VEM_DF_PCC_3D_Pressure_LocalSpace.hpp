ffe0fae933977f7129d3305eaed978341eb9ec66
Andrea Borio <andrea.borio@polito.it>
2025-03-13 19:33:04 +0100

    Docs of I_VEM_DF_PCC_{2,3}D_Pressure_LocalSpace

--- PolyDiM/src/VEM/DF_PCC/3D/I_VEM_DF_PCC_3D_Pressure_LocalSpace.hpp
+++ PolyDiM/src/VEM/DF_PCC/3D/I_VEM_DF_PCC_3D_Pressure_LocalSpace.hpp
@@ -16,21 +16,42 @@ namespace DF_PCC
 class I_VEM_DF_PCC_3D_Pressure_LocalSpace
 {
   public:
+    /// \brief Class destructor.
     virtual ~I_VEM_DF_PCC_3D_Pressure_LocalSpace()
     {
     }
 
+    /// \brief Compute data of VEM space on a polyhedron.
+    /// \param reference_element_data Data of the reference element of the VEM space.
+    /// \param polyhedron The geometry of the polyhedron.
+    /// \return A data structure containing the information about the local space.
     virtual VEM_DF_PCC_3D_Pressure_LocalSpace_Data CreateLocalSpace(const VEM_DF_PCC_3D_Pressure_ReferenceElement_Data &reference_element_data,
                                                                     const VEM_DF_PCC_3D_Polyhedron_Geometry &polyhedron) const = 0;
 
+    /// \brief Compute values of basis functions at default quadrature points.
+    /// \param localSpace Data of the local space.
+    /// \return A matrix containing the values of a basis function on each column.
     virtual Eigen::MatrixXd ComputeBasisFunctionsValues(const VEM_DF_PCC_3D_Pressure_LocalSpace_Data &localSpace) const = 0;
 
+    /// \brief Compute values of basis functions at given points.
+    /// \param reference_element_data Data of the reference element.
+    /// \param localSpace Data of the local space.
+    /// \param points Evaluation points.
+    /// \return A matrix containing the values of a basis function on each column.
     virtual Eigen::MatrixXd ComputeBasisFunctionsValues(const VEM_DF_PCC_3D_Pressure_ReferenceElement_Data &reference_element_data,
                                                         const VEM_DF_PCC_3D_Pressure_LocalSpace_Data &localSpace,
                                                         const Eigen::MatrixXd &points) const = 0;
 
+    /// \brief Compute values of the polynomial basis at default quadrature points.
+    /// \param localSpace Data of the local space.
+    /// \return A matrix containing the values of a basis function on each column.
     virtual Eigen::MatrixXd ComputePolynomialsValues(const VEM_DF_PCC_3D_Pressure_LocalSpace_Data &localSpace) const = 0;
 
+    /// \brief Compute values of the polynomial basis at given points.
+    /// \param reference_element_data Data of the reference element of the VEM space.
+    /// \param localSpace Data of the local space.
+    /// \param points Evaluation points.
+    /// \return A matrix containing the values of a basis function on each column.
     virtual Eigen::MatrixXd ComputePolynomialsValues(const VEM_DF_PCC_3D_Pressure_ReferenceElement_Data &reference_element_data,
                                                      const VEM_DF_PCC_3D_Pressure_LocalSpace_Data &localSpace,
                                                      const Eigen::MatrixXd &points) const = 0;
