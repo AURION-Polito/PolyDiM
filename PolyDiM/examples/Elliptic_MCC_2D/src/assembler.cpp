88a704f7c2a9cdf1e231599b2d006c9b3896668b
Fabio Vicini <90675116+fvicini@users.noreply.github.com>
2025-05-21 15:53:44 +0200

    Add license (#14)
    * align gedim
    * add: license files
    * fix: assembler character

--- PolyDiM/examples/Elliptic_MCC_2D/src/assembler.cpp
+++ PolyDiM/examples/Elliptic_MCC_2D/src/assembler.cpp
@@ -1,4 +1,15 @@
-﻿#include "assembler.hpp"
+// _LICENSE_HEADER_
+//
+// Copyright (C) 2019 - 2025.
+// Terms register on the GPL-3.0 license.
+//
+// This file can be redistributed and/or modified under the license terms.
+//
+// See top level LICENSE file for more details.
+//
+// This file can be used citing references in CITATION.cff file.
+
+#include "assembler.hpp"
 
 #include "EllipticEquation.hpp"
 #include "VEM_MCC_2D_LocalSpace_Data.hpp"
