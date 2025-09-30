9217eaf345c7de3e335e37f4de1cb5cefeb9b161
gioanateora <gioana.teora@polito.it>
2025-09-30 13:52:26 +0200

    verbose

--- PolyDiM/src/Utilities/Inertia_Utilities.hpp
+++ PolyDiM/src/Utilities/Inertia_Utilities.hpp
@@ -37,13 +37,13 @@ struct Inertia_Utilities final
                           const Eigen::Vector3d &centroid,
                           const double &diameter,
                           const std::vector<Eigen::Matrix3d> &triangulation_vertices,
-                          Inertia_Data &inertia_data) const;
+                          Polydim::Utilities::Inertia_Utilities::Inertia_Data &inertia_data) const;
 
     void InertiaMapping3D(const Eigen::MatrixXd &vertices,
                           const Eigen::Vector3d &centroid,
                           const double &diameter,
                           const std::vector<Eigen::MatrixXd> &tetrahedrons_vertices,
-                          Inertia_Data &inertia_data) const;
+                          Polydim::Utilities::Inertia_Utilities::Inertia_Data &inertia_data) const;
 };
 
 } // namespace Utilities
