63e88499f5849942fc9f06383a0ffd8b9df5442a
fabio.vicini <fabio.vicini@polito.it>
2025-10-14 11:08:38 +0200

    still

--- PolyDiM/examples/Parabolic_PCC_2D/integration_test.py
+++ PolyDiM/examples/Parabolic_PCC_2D/integration_test.py
@@ -3,6 +3,11 @@ import csv
 import matplotlib.pyplot as plt
 import numpy as np
 
+def time_order(theta):                    
+    if theta == 0.5:
+        return 2
+    else:
+        return 1
 
 def run_program(program_folder,
                 program_path,
@@ -43,14 +48,9 @@ def run_program(program_folder,
     output_file = os.path.join(program_folder,
                                "terminal.log")
     
-    time_order = 1                       
-    if theta == 0.5:
-    	time_order = 2
-    	
-
     run_label = "MethodType {0}".format(method_type)
     run_label += " SpaceMethodOrder {0}".format(method_order)
-    run_label += " TimeMethodOrder {0}".format(time_order)
+    run_label += " TimeMethodOrder {0}".format(time_order(theta))
     run_label += " TestType {0}".format(test_type)
     run_label += " MeshGenerator {0}".format(mesh_generator)
     run_label += " NumRefinement {0}".format(num_ref)
@@ -92,15 +92,20 @@ def import_errors(export_path, method_type, method_order, time_order, test_type)
     return errors
 
 
-def test_errors(errors,
-                method_order,
-                tol):
+def test_space_errors(errors,
+                      method_order,
+                      row_number,
+                      tol):
     num_rows = len(errors)
 
     if num_rows == 2:
         print("Num. Ref. 1: ", abs(errors[1][1]) / abs(errors[1][3]), abs(errors[1][2]) / abs(errors[1][4]))
         assert abs(errors[1][1]) < tol * abs(errors[1][3])
         assert abs(errors[1][2]) < tol * abs(errors[1][4])
+    elif row_number > 0:
+        print("Num. Ref. 1: ", abs(errors[row_number][1]) / abs(errors[row_number][3]), abs(errors[row_number][2]) / abs(errors[row_number][4]))
+        assert abs(errors[row_number][1]) < tol * abs(errors[row_number][3])
+        assert abs(errors[row_number][2]) < tol * abs(errors[row_number][4])
     else:
         errors = np.array(errors[1:])
         slope_L2 = np.polyfit(np.log(errors[:, 0]), np.log(errors[:, 1]), 1)[0]
@@ -125,29 +130,30 @@ if __name__ == "__main__":
     test_type = 1
     mesh_generator = 1
     mesh_max_area = 0.0
-    method_types = [0, 1, 2, 3]
+    method_types = [0, 1, 3]
     method_orders = [1, 2, 3]
     thetas = [0, 1, 0.5]
     for theta in thetas:
         for method_type in method_types:
-    	    for method_order in method_orders:
-		        export_path = run_program(program_folder,
-		                                  program_path,
-		                                  "Run_MG{0}".format(mesh_generator),
-		                                  method_type,
-		                                  method_order,
-		                                  test_type,
-		                                  mesh_generator,
-		                                  0,
-		                                  mesh_max_area=mesh_max_area,
-		                                  theta=theta)
-		        errors = import_errors(export_path, method_type, method_order, test_type)
-		        test_errors(errors,
-		                    method_order,
-		                    tol)
+            for method_order in method_orders:
+                export_path = run_program(program_folder,
+                                          program_path,
+                                          "Run_MG{0}".format(mesh_generator),
+                                          method_type,
+                                          method_order,
+                                          test_type,
+                                          mesh_generator,
+                                          0,
+                                          mesh_max_area=mesh_max_area,
+                                          theta=theta)
+                errors = import_errors(export_path, method_type, method_order, time_order(theta), test_type)
+                test_space_errors(errors,
+                                  method_order,
+                                  2,
+                                  tol)
 
-		        if remove_folder:
-		            os.system("rm -rf " + os.path.join(program_folder, export_path))
+                if remove_folder:
+                    os.system("rm -rf " + os.path.join(program_folder, export_path))
 
     test_type = 1
     mesh_generator = 0
@@ -156,54 +162,57 @@ if __name__ == "__main__":
     method_orders = [1, 2, 3]
     thetas = [0, 1, 0.5]
     for theta in thetas:
-    	for method_type in method_types:
-    	    for method_order in method_orders:
-		        export_path = run_program(program_folder,
-		                                  program_path,
-		                                  "Run_MG{0}".format(mesh_generator),
-		                                  method_type,
-		                                  method_order,
-		                                  test_type,
-		                                  mesh_generator,
-		                                  0,
-		                                  mesh_max_area=mesh_max_area,
-		                                  theta=theta)
-		        errors = import_errors(export_path, method_type, method_order, test_type)
-		        test_errors(errors,
-		                    method_order,
-		                    tol)
+        for method_type in method_types:
+            for method_order in method_orders:
+                export_path = run_program(program_folder,
+                                          program_path,
+                                          "Run_MG{0}".format(mesh_generator),
+                                          method_type,
+                                          method_order,
+                                          test_type,
+                                          mesh_generator,
+                                          0,
+                                          mesh_max_area=mesh_max_area,
+                                          theta=theta)
+                errors = import_errors(export_path, method_type, method_order, time_order(theta), test_type)
+                test_space_errors(errors,
+                                  method_order,
+                                  2,
+                                  tol)
 
-		        if remove_folder:
-		            os.system("rm -rf " + os.path.join(program_folder, export_path))
+                if remove_folder:
+                    os.system("rm -rf " + os.path.join(program_folder, export_path))
 
     test_type = 4
     mesh_generator = 5
-    method_types = [0, 1, 2, 3]
+    method_types = [0, 1, 3]
     mesh_max_areas = [1./16., 1./64., 1./(16.*16), 1./(32.*32)]
     method_orders = [1, 2]
     thetas = [0, 1, 0.5]
     for theta in thetas:
-    	for method_type in method_types:
-    	    for method_order in method_orders:
-		        num_ref = 0
-		        for mesh_max_area in mesh_max_areas:
-		            export_path = run_program(program_folder,
-		                                      program_path,
-		                                      "Run_MG{0}".format(mesh_generator),
-		                                      method_type,
-		                                      method_order,
-		                                      test_type,
-		                                      mesh_generator,
-		                                      num_ref,
-		                                      mesh_max_area=mesh_max_area,
-		                                      theta=theta)
-		            num_ref += 1
+        for method_type in method_types:
+            for method_order in method_orders:
+                num_ref = 0
+                for mesh_max_area in mesh_max_areas:
+                    export_path = run_program(program_folder,
+                                              program_path,
+                                              "Run_MG{0}".format(mesh_generator),
+                                              method_type,
+                                              method_order,
+                                              test_type,
+                                              mesh_generator,
+                                              num_ref,
+                                              mesh_max_area=mesh_max_area,
+                                              theta=theta)
+                    num_ref += 1
 
-		        errors = import_errors(export_path, method_type, method_order, test_type)
-		        test_errors(errors,
-		                    method_order,
-		                    tol)
-		        if remove_folder:
-		            os.system("rm -rf " + os.path.join(program_folder, export_path))
+                errors = import_errors(export_path, method_type, method_order, time_order(theta), test_type)
+                test_space_errors(errors,
+                                  method_order,
+                                  0,
+                                  tol)
+                
+                if remove_folder:
+                    os.system("rm -rf " + os.path.join(program_folder, export_path))
 
     print("TESTS SUCCESS")
