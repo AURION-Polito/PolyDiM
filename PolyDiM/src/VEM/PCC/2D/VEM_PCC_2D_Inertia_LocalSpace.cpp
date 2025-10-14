5f3e7752f396ddb3039fd3b2d528a225fa5e6f8d
fabio.vicini <fabio.vicini@polito.it>
2025-10-14 09:47:01 +0200

    fix: inertia

--- PolyDiM/src/VEM/PCC/2D/VEM_PCC_2D_Inertia_LocalSpace.cpp
+++ PolyDiM/src/VEM/PCC/2D/VEM_PCC_2D_Inertia_LocalSpace.cpp
@@ -285,6 +285,7 @@ void VEM_PCC_2D_Inertia_LocalSpace::InitializeProjectorsComputation(const VEM_PC
     localSpace.NumProjectorBasisFunctions = reference_element_data.Monomials.NumMonomials;
 
     localSpace.Nkm1 = localSpace.NumProjectorBasisFunctions - reference_element_data.Order - 1;
+    localSpace.Nkm2 = (reference_element_data.Order - 1) * reference_element_data.Order / 2;
 
     // Compute Vandermonde matrices.
     localSpace.VanderInternal =
