ca5a9b4f6322b9f7014a53d022a147d696cdd60f
fabio.vicini <fabio.vicini@polito.it>
2026-02-11 15:56:53 +0100

    fix: ordering

--- PolyDiM/src/FEM/PCC/2D/FEM_Triangle_PCC_2D_LocalSpace.cpp
+++ PolyDiM/src/FEM/PCC/2D/FEM_Triangle_PCC_2D_LocalSpace.cpp
@@ -96,15 +96,16 @@ FEM_Triangle_PCC_2D_LocalSpace_Data FEM_Triangle_PCC_2D_LocalSpace::CreateLocalS
 std::vector<MatrixXd> FEM_Triangle_PCC_2D_LocalSpace::MapDerivativeValues(const FEM_Triangle_PCC_2D_LocalSpace_Data &local_space,
                                                                           const std::vector<Eigen::MatrixXd> &referenceDerivateValues) const
 {
+    std::vector<Eigen::MatrixXd> basis_functions_mapped(2);
+    basis_functions_mapped.at(0).noalias() = local_space.MapData.BInv(0, 0) * referenceDerivateValues.at(0);
+    basis_functions_mapped.at(0).noalias() += local_space.MapData.BInv(1, 0) * referenceDerivateValues.at(1);
+
+    basis_functions_mapped.at(1).noalias() = local_space.MapData.BInv(0, 1) * referenceDerivateValues.at(0);
+    basis_functions_mapped.at(1).noalias() += local_space.MapData.BInv(1, 1) * referenceDerivateValues.at(1);
+
     std::vector<Eigen::MatrixXd> basis_functions_reordered(2);
-    basis_functions_reordered.at(0).noalias() = local_space.MapData.BInv(0, 0) * referenceDerivateValues.at(0);
-    basis_functions_reordered.at(0).noalias() += local_space.MapData.BInv(1, 0) * referenceDerivateValues.at(1);
-
-    basis_functions_reordered.at(1).noalias() = local_space.MapData.BInv(0, 1) * referenceDerivateValues.at(0);
-    basis_functions_reordered.at(1).noalias() += local_space.MapData.BInv(1, 1) * referenceDerivateValues.at(1);
-
-    basis_functions_reordered.at(0) =  basis_functions_reordered.at(0)(Eigen::all, local_space.DofsMeshOrder);
-    basis_functions_reordered.at(1) =  basis_functions_reordered.at(1)(Eigen::all, local_space.DofsMeshOrder);
+    basis_functions_reordered.at(0) =  basis_functions_mapped.at(0)(Eigen::all, local_space.DofsMeshOrder);
+    basis_functions_reordered.at(1) =  basis_functions_mapped.at(1)(Eigen::all, local_space.DofsMeshOrder);
 
     return basis_functions_reordered;
 }
